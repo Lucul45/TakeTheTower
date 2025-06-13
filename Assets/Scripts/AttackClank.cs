1bda959107a4d00b1a52edf42a20b60c45fee91c
Lucul45 <71066283+Lucul45@users.noreply.github.com>
2025-06-13 13:37:07 +0200

    ajout asset music

--- Assets/Scripts/AttackClank.cs
+++ Assets/Scripts/AttackClank.cs
@@ -17,7 +17,7 @@ public class AttackClank : MonoBehaviour
         if (collision != null)
         {
             Debug.Log(collision.gameObject.name);
-            if (collision.gameObject.name == "AttackHitbox")
+            /*if (/*collision.gameObject.name == "AttackHitbox" _stateMachineManager.OtherPlayer.GetComponent<PlayerStateMachineManager>().CanClank)
             {
                 StartCoroutine(_stateMachineManager.Clank());
                 StartCoroutine(_stateMachineManager.OtherPlayer.GetComponent<PlayerStateMachineManager>().Clank());
@@ -47,7 +47,27 @@ public class AttackClank : MonoBehaviour
                         StartCoroutine(_damageManager.Freeze());
                     }
                 }
+            }*/
+            if (_stateMachineManager.CurrentAttack != null && ((tag == "AttackP1" && collision.transform.parent.gameObject.tag == "Player2") || (tag == "AttackP2" && collision.transform.parent.gameObject.tag == "Player1")))
+            {
+                if (_stateMachineManager.OtherPlayer.GetComponent<PlayerStateMachineManager>().CanClank)
+                {
+                    StartCoroutine(_stateMachineManager.Clank());
+                    StartCoroutine(_stateMachineManager.OtherPlayer.GetComponent<PlayerStateMachineManager>().Clank());
+                }
+                else if (!_stateMachineManager.OtherPlayer.GetComponent<PlayerStateMachineManager>().IsParrying)
+                {
+                    _stateMachineManager.OtherPlayer.GetComponent<PlayerStateMachineManager>().ChangeState(EPlayerState.HURT);
+                    _stateMachineManager.OtherPlayer.GetComponent<PlayerStateMachineManager>().PlayerDamageManager.TakeDamage(_stateMachineManager.CurrentAttack.AttackDamage);
+                    if (!_damageManager.FreezeEnabled)
+                    {
+                        StartCoroutine(_damageManager.Freeze());
+                    }
+                    Debug.Log(_stateMachineManager.CanClank + "/" + _stateMachineManager.OtherPlayer.GetComponent<PlayerStateMachineManager>().CanClank);
+                }
             }
+            
+            
         }
     }
 }
